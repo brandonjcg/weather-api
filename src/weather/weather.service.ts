0a35262df6f519dfb74e7e1bde8b0ca3acb5a62a
Brandon Castillo <brajoel98@gmail.com>
2024-07-27 00:38:11 -0700

    feat(cronjob): :rocket: config cronjob

--- src/weather/weather.service.ts
+++ src/weather/weather.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { Cron, CronExpression } from '@nestjs/schedule';
 
 @Injectable()
-export class WeatherService {}
+export class WeatherService {
+  @Cron(CronExpression.EVERY_SECOND)
+  async fetchWeatherData() {
+    try {
+      if (Math.random() < 0.2) console.error('🔴 Error fetching weather data');
+
+      console.log('🟢 Fetch successful');
+    } catch (error) {
+      console.error('Error fetching weather data:', error);
+    }
+  }
+}
