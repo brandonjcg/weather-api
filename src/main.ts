e4210de46e1e527559b19439d71887738a0df186
Brandon Castillo <brajoel98@gmail.com>
2024-07-27 00:01:49 -0700

    feat(service): :technologist: add prefix and config of validators

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe, Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+
+  const globalPrefix = 'api/v1';
+  app.setGlobalPrefix(globalPrefix);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
+  const PORT = process.env.PORT || 3000;
+  await app.listen(PORT);
+
+  Logger.log(
+    `🚀 Application is running on: http://localhost:${PORT}/${globalPrefix}`,
+  );
 }
 bootstrap();
